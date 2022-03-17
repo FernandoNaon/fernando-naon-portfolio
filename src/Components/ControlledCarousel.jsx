9a042b8a8eb8eed179f4741a4ccb6928f81d7cd3
Fernando Naon <74456529+FernandoNaon@users.noreply.github.com>
2022-03-17 13:04:24 +0100

    updated proj

--- src/Components/ControlledCarousel.jsx
+++ src/Components/ControlledCarousel.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import recipe1 from "../Assets/projects/recipe1.png";
-import recipe2 from "../Assets/projects/recipe2.png";
-import recipe3 from "../Assets/projects/recipe3.png";
 
-const ControlledCarousel = () => {
+const ControlledCarousel = ({ img1, img2, img3 }) => {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
@@ -14,16 +11,16 @@ const ControlledCarousel = () => {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img className="d-block w-100" src={recipe1} alt="First slide" />
+        <img className="d-block w-100" src={img1} alt="First slide" />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={recipe2} alt="Second slide" />
+        <img className="d-block w-100" src={img2} alt="Second slide" />
 
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={recipe3} alt="Third slide" />
+        <img className="d-block w-100" src={img3} alt="Third slide" />
 
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ControlledCarousel from "./ControlledCarousel";
 import style from "./Projects.module.css";
+import recipe1 from "../Assets/projects/recipe1.png";
+import recipe2 from "../Assets/projects/recipe2.png";
+import recipe3 from "../Assets/projects/recipe3.png";
+import groupProj1 from "../Assets/project/groupProj1.png";
+import groupProj2 from "../Assets/projects/groupProj2.png";
+import groupProj3 from "../Assets/projects/groupProj3.png";
 
 const Projects = () => {
   return (
@@ -8,7 +14,7 @@ const Projects = () => {
       <div className={style.cardContainer}>
         <div className={style.card}>
           <div className={style.carousel}>
-            <ControlledCarousel />
+            <ControlledCarousel img1={recipe1} img2={recipe2} img3={recipe3} />
           </div>
           <div className={style.title}>
             <h3>Recipedia</h3>
@@ -31,6 +37,45 @@ const Projects = () => {
             <a
               className={style.link}
               href="https://recipedia-frontend.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Demo
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={style.cardContainer}>
+        <div className={style.card}>
+          <div className={style.carousel}>
+            <ControlledCarousel
+              img1={groupProj1}
+              img2={groupProj2}
+              img3={groupProj3}
+            />
+          </div>
+          <div className={style.title}>
+            <h3>A Sala Llena</h3>
+          </div>
+          <div className={style.description}>
+            <p>
+              Full-Stack app in where we used the scrum project management
+              methodology with weekly sprints. It's a last minute ticket sales
+              app for theaters, with a dynamic discount based on time,
+            </p>
+          </div>
+          <div className={style.linksContainer}>
+            <a
+              className={style.link}
+              href="https://github.com/FernandoNaon/front-groupProj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Ropository
+            </a>
+            <a
+              className={style.link}
+              href="https://front-pg.vercel.app/"
               target="_blank"
               rel="noreferrer"
             >
